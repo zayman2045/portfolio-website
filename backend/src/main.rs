0a6f5f1530046861b26c4406927e0d524f290034
zayman2045 <xavierorlandogriffith@gmail.com>
2023-09-06 00:15:43 -0400

    Create first HTML view

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World! Hey there" }));
+    let app = Router::new().route("/", get(|| async { "Hello Backend!" }));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,12 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <h1> {"Hello Frontend! "} </h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
