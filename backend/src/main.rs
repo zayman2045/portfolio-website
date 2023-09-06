4a72d778f7e48b3ed4661e6c3bdf082d592c64bf
zayman2045 <xavierorlandogriffith@gmail.com>
2023-09-06 18:41:53 -0400

    Define yew router in frontend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,14 @@
+// use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello Backend!" }));
 
-    //let addr = Sock
+    //let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,26 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-#[function_component]
-fn App() -> Html {
+// Create yew router
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+}
+
+// Dispatch routes
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Hello Frontend!!!" }</h1> },
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
     html! {
-        <h1> {"Hello Frontend! "} </h1>
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
