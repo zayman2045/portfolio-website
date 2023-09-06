bccab078ba07256be88c0e56a3f67d8130430d75
zayman2045 <xavierorlandogriffith@gmail.com>
2023-09-06 18:01:57 -0400

    Define workspace in Cargo.toml file at the root of the project

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,8 @@ use axum::{routing::get, Router};
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello Backend!" }));
 
+    //let addr = Sock
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
