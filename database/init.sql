73385e37cf466c7b4cc0a93b0712ac40e6f7d9ef
zayman2045 <xavierorlandogriffith@gmail.com>
2023-09-28 19:23:30 -0400

    Spin up postgres database using docker compose.

--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id          SERIAL PRIMARY KEY,
   username    VARCHAR(64) NOT NULL UNIQUE,
-  password    VARCHAR(64) NOT NULL,
+  password    VARCHAR(64) NOT NULL
 );
 
 INSERT INTO users (username, password) VALUES ('sample_user', 'sample_password');
