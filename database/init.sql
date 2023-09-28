97a7bd8316c2d3841d102f054bb34a9f33798586
zayman2045 <xavierorlandogriffith@gmail.com>
2023-09-28 18:17:03 -0400

    Simplify user table by removing token.

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
   id          SERIAL PRIMARY KEY,
   username    VARCHAR(64) NOT NULL UNIQUE,
   password    VARCHAR(64) NOT NULL,
-  token       TEXT DEFAULT NULL
 );
 
-INSERT INTO users (username, password) VALUES ('fake_user', 'fake_password');
+INSERT INTO users (username, password) VALUES ('sample_user', 'sample_password');
