5bb46b0d6c1653cf8081d3202dafddf1d2cb28c0
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-07 11:17:49 -0400

    Coordinate frontend, backend and database servers to all spin up from a single docker-compose.yaml file.

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -8,13 +8,13 @@ pub async fn run() -> Result<(), DbErr> {
     let app = routes::create_router().await;
 
     // Connect to database
-    // let _db = match Database::connect(DATABASE_URL).await {
-    //     Ok(db) => db,
-    //     Err(error) => {
-    //         eprintln!("Error connecting to the database: {:?}", error);
-    //         panic!();
-    //     }
-    // };
+    let _db = match Database::connect(DATABASE_URL).await {
+        Ok(db) => db,
+        Err(error) => {
+            eprintln!("Error connecting to the database: {:?}", error);
+            panic!();
+        }
+    };
     
     // Serve API
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
