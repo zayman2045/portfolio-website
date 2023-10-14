fa6003b9710b1e543f78b235f9c53d81a4d9cf77
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-14 17:17:59 -0400

    Improvements to UI.

--- frontend/src/components/molecules/nav_bar.rs
+++ frontend/src/components/molecules/nav_bar.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::Link;
 
+use crate::components::molecules::link_button::LinkButton;
 use crate::router::Route;
 
 // Link that wraps a button
@@ -10,15 +11,18 @@ pub fn link_button() -> Html {
         <nav class="navbar">
             <div class="navbar-container">
                 <div class="navbar-left">
-                <h1>{"Xavier Griffith"}</h1>
-                <h2>{"Specializing in Rust Development"}</h2>
+                    <Link<Route> to={Route::Home}>
+                        <img class={"navbar-logo"} src={"img/xg-logo.jpeg"} alt={"XG Logo"} />
+                    </Link<Route>>
+                    <h1>{"Xavier Griffith"}</h1>
+                    <h2>{"Specializing in Rust Development"}</h2>
                 </div>
                 <div class="navbar-right">
                 // TODO: Conditionally render the login and sign up buttons if the user is not logged in, unless they are on a application page (Stargaze, Funder, Ello)
                 // TODO: Link to the user's profile if the user is logged in
-                <Link<Route> to={Route::Home}>
-                    <img class={"navbar-logo"} src={"img/xg-logo.jpeg"} alt={"XG Logo"} />
-                </Link<Route>>
+                <LinkButton route={Route::Login} label={"About Me".to_string()} kind={"button".to_string()} />
+                <LinkButton route={Route::Login} label={"About This Site".to_string()} kind={"button".to_string()} />
+                <LinkButton route={Route::Login} label={"Projects".to_string()} kind={"button".to_string()} />
                 </div>
             </div>
         </nav>
