2e2c91c49fca9a39fd2ae2911fd984a56d74343a
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-14 13:22:05 -0400

    Add home link wrapper around logo.

--- frontend/src/components/molecules/nav_bar.rs
+++ frontend/src/components/molecules/nav_bar.rs
@@ -1,6 +1,6 @@
 use yew::prelude::*;
+use yew_router::prelude::Link;
 
-use super::link_button::LinkButton;
 use crate::router::Route;
 
 // Link that wraps a button
@@ -10,13 +10,15 @@ pub fn link_button() -> Html {
         <nav class="navbar">
             <div class="navbar-container">
                 <div class="navbar-left">
-                    <h1>{"Xavier Griffith"}</h1>
-                    <h2>{"Specializing in Rust Development"}</h2>
+                <h1>{"Xavier Griffith"}</h1>
+                <h2>{"Specializing in Rust Development"}</h2>
                 </div>
                 <div class="navbar-right">
                 // TODO: Conditionally render the login and sign up buttons if the user is not logged in, unless they are on a application page (Stargaze, Funder, Ello)
                 // TODO: Link to the user's profile if the user is logged in
-                    <LinkButton route={Route::Home} label={"User".to_string()} kind={"button".to_string()} />
+                <Link<Route> to={Route::Home}>
+                    <img class={"navbar-logo"} src={"img/xg-logo.jpeg"} alt={"XG Logo"} />
+                </Link<Route>>
                 </div>
             </div>
         </nav>
