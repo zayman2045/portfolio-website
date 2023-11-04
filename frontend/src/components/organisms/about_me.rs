e80bcd43e0ac8c54010b157da50b94a933f500b5
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-04 07:27:47 -0400

    Add personal information to my about me page.

--- frontend/src/components/organisms/about_me.rs
+++ frontend/src/components/organisms/about_me.rs
@@ -15,7 +15,11 @@ pub fn stargaze() -> Html {
                 <NavBar />
                 <h1> {"About Me"} </h1>
                 <div class={"content-container"}>
-                    <p>{"I am a software developer proficient in the Rust programming language."}</p>
+                    <p>{"Hello! I am a software developer with a profound enthusiasm for the world of technology and innovation. I'm a Biomedical Engineering graduate from The Johns Hopkins University, originally from Baltimore, Maryland, and now living in Miami, Florida. My academic journey equipped me with a diverse programming toolkit, including C, Java, Python, JavaScript, HTML, CSS, and Matlab. However, it was Rust's promise of performance and reliability that captured my professional dedication.
+
+                    My passion for Rust is not just about the language itself, but about the potential it holds in areas that intrigue me the most: the precision of biotechnology, the dynamism of cryptocurrency markets, the challenge of mastering foreign languages, and the immersive experiences of video games. I approach learning with a voracious appetite, ever-eager to explore how the robustness of Rust can be leveraged to solve complex problems in these domains.
+                    
+                    As a developer, my goal is to intertwine my technical skills with my interests, creating solutions that are not only efficient and safe but also groundbreaking. With each line of code, I strive to bring a blend of engineering acumen and creative insight, driven by a belief that software is a pivotal tool in sculpting the innovations of tomorrow."}</p>
                     <img src={"img/developer-photo.jpeg"} alt={"A photograph of the developer"} />
                 </div>
                 <ContactFooter />
