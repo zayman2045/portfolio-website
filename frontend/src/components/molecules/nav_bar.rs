b605a91d146a540801b0176d25e0a6720f13f93b
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-08 00:44:24 -0500

    Adjust the <a> wrappers to better fit underlyng content.

--- frontend/src/components/molecules/nav_bar.rs
+++ frontend/src/components/molecules/nav_bar.rs
@@ -1,7 +1,6 @@
 use yew::prelude::*;
 use yew_router::prelude::Link;
 
-use crate::components::molecules::link_button::LinkButton;
 use crate::router::Route;
 
 // Navigation bar that contains a logo and links to other pages
@@ -18,9 +17,15 @@ pub fn link_button() -> Html {
                     <h2>{"Specializing in Rust Development"}</h2>
                 </div>
                 <div class="navbar-right">
-                    <LinkButton route={Route::AboutMe} label={"Meet The Developer".to_string()} kind={"button".to_string()} />
-                    <LinkButton route={Route::AboutSite} label={"Site Overview".to_string()} kind={"button".to_string()} />
-                    <LinkButton route={Route::AboutProjects} label={"My Projects".to_string()} kind={"button".to_string()} />
+                    <Link<Route> to={Route::AboutMe}>
+                        {"Meet The Developer"}
+                    </Link<Route>>
+                    <Link<Route> to={Route::AboutSite}>
+                        {"Site Overview"}
+                    </Link<Route>>
+                    <Link<Route> to={Route::AboutProjects}>
+                        {"My Projects"}
+                    </Link<Route>>
                 </div>
             </div>
         </nav>
