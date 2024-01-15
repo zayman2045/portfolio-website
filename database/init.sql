533d023bae5ede298819d52a07b9944ea3b1aae9
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-15 10:30:39 -0500

    Create missions table within the database.

--- database/init.sql
+++ database/init.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (username, password) VALUES ('sample_user', 'sample_password');
+
+CREATE TABLE IF NOT EXISTS missions (
+  id      SERIAL PRIMARY KEY,
+  title   VARCHAR(255) NOT NULL,
+  content TEXT
+);
+
+INSERT INTO missions (title, content) VALUES ('sample_mission', 'sample_content');
