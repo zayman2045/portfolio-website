e6291d4cafc06e76f33fc3d31c2f6008c6560a5b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-17 13:04:17 -0500

    Expand mission store with new store type for handling a single mission.

--- frontend/src/stores/mission_store.rs
+++ frontend/src/stores/mission_store.rs
@@ -3,13 +3,24 @@
 use serde::{Deserialize, Serialize};
 use yewdux::prelude::*;
 
+
+
+/// A single mission.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Store, Serialize, Deserialize)]
+pub struct MissionStore {
+    pub id: i32,
+    pub user_id: i32,
+    pub title: String,
+    pub content: Option<String>,
+}
+
 /// The user's list of missions.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Store)]
-pub struct MissionStore {
+pub struct MissionListStore {
     pub missions: Option<Vec<Mission>>,
 }
 
-/// A single mission.
+/// Helper type for the user's list of missions.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Mission {
     pub id: i32,
