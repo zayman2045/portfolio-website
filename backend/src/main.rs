40ae9db91cbe68721ed6eed13ba0983b29d0ffe2
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-26 15:01:42 -0500

    Prepare for development

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
 //! Entrypoint for the backend server.
 
 use backend::run;
-// use dotenv::dotenv; // Uncomment in development
+use dotenv::dotenv; // Only Uncomment in Development
 use std::env;
 
 /// Runs the backend server.
 #[tokio::main]
 async fn main() {
-    // dotenv().ok(); // Uncomment in development
+    dotenv().ok(); // Only Uncomment in Development
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
     run(database_url).await.unwrap();
 }
