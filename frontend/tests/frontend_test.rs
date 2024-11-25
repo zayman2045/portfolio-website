c5f95c01536d5c3394132735256df6e0d862fb81
zayman2045 <xavierorlandogriffith@gmail.com>
2024-11-25 08:18:22 -0500

    Comment out the all targets test

--- frontend/tests/frontend_test.rs
+++ frontend/tests/frontend_test.rs
@@ -7,7 +7,7 @@ fn pass() {
 
 // On a target other then `wasm32-unknown-unknown`, the `#[test]` attribute
 // will be used instead, allowing this test to run on any target.
-#[wasm_bindgen_test(unsupported = test)]
-fn pass_all_targets() {
-    assert_eq!(2, 2);
-}
+// #[wasm_bindgen_test(unsupported = test)]
+// fn pass_all_targets() {
+//     assert_eq!(2, 2);
+// }
