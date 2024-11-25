bf78bef2d333dcc9427955f46372baa0202d62fc
zayman2045 <xavierorlandogriffith@gmail.com>
2024-11-25 08:11:40 -0500

    Update frontend tests to use wasm_bindgen_test for improved compatibility

--- frontend/tests/frontend_test.rs
+++ frontend/tests/frontend_test.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_add() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen_test]
+fn pass() {
+    assert_eq!(1, 1);
+}
+
+// On a target other then `wasm32-unknown-unknown`, the `#[test]` attribute
+// will be used instead, allowing this test to run on any target.
+#[wasm_bindgen_test(unsupported = test)]
+fn pass_all_targets() {
+    assert_eq!(2, 2);
 }
