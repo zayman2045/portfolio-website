9d287fdbcee143b3a5e4d646bce63167bfa463dd
zayman2045 <xavierorlandogriffith@gmail.com>
2024-12-04 21:49:54 -0500

    Define a tests module to hold frontend tests

--- frontend/tests/frontend_test.rs
+++ frontend/tests/frontend_test.rs
@@ -1,13 +1,9 @@
-use wasm_bindgen_test::*;
+#[cfg(test)]
+pub mod tests {
+    use wasm_bindgen_test::*;
 
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1, 1);
+    #[wasm_bindgen_test]
+    fn pass() {
+        assert_eq!(1, 1);
+    }
 }
-
-// On a target other then `wasm32-unknown-unknown`, the `#[test]` attribute
-// will be used instead, allowing this test to run on any target.
-// #[wasm_bindgen_test(unsupported = test)]
-// fn pass_all_targets() {
-//     assert_eq!(2, 2);
-// }
